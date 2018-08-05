746d579398fee0bc428a724c5f305c7d6bae4f72
Mohamed Lamouchi <lms.lamouchi@gmail.com>
2018-08-05 03:29:55 -0400

    separate tmux

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@
 # Essentials
 sudo apt-get install git cmake build-essential python-dev -y
 
-# Tmux
-mkdir -p ~/.tmux
-git clone https://github.com/seebi/tmux-colors-solarized ~/.tmux/tmux-colors-solarized
-cat ~/.tmux/tmux-colors-solarized/tmuxcolors-256.conf >> ~/.tmux.conf
-
 # YouCompleteMe
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 
