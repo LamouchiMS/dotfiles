4547b00a4bae90c164cc37c226a2ce0768b5ea28
Mohamed Lamouchi <lms.lamouchi@gmail.com>
2018-08-05 03:31:12 -0400

    separate ycm vim

--- setup.sh
+++ setup.sh
@@ -3,14 +3,5 @@
 # Essentials
 sudo apt-get install git cmake build-essential python-dev -y
 
-# YouCompleteMe
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-
-# YouCompleteMe setup
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py --all
-cd
-
 reset
 
